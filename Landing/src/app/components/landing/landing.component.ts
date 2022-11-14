549e312c61b7ef28f899aa04a5e1d444263f3789
benie254 <davinci.monalissa@gmail.com>
2022-11-14 10:08:44 +0300

    update notifications

--- Landing/src/app/components/landing/landing.component.ts
+++ Landing/src/app/components/landing/landing.component.ts
@@ -42,6 +42,57 @@ export class LandingComponent implements OnInit {
     //   }
     // })
   }
+  order(){
+    Notiflix.Confirm.show(
+      'Confirm purchase',
+      "Are you sure you want to place this order?",
+      "I'm sure",
+      "Take me back",
+      () => {
+        Notiflix.Report.success(
+          "Success!",
+          "Your order was placed successfully. We will email you more instructions and share a progress report as soon as we get things going. Hang tight.",
+          "Thanks",
+        )
+      },
+      () => {
+        Notiflix.Report.success(
+          "Aborted!",
+          "Your order placement was aborted successfully. If you didn't mean to cancel, you can always place a new order.",
+          'Great',
+        )
+      }
+    )
+  }
+  book(){
+    Notiflix.Confirm.show(
+      'Confirm booking',
+      "Are you sure you want to book this order?",
+      "I'm sure",
+      "Take me back",
+      () => {
+        Notiflix.Report.success(
+          "Success!",
+          "You successfully booked a copy of this package. We will email you more instructions as soon as we get things going. Hang tight.",
+          "Thanks",
+        )
+      },
+      () => {
+        Notiflix.Report.success(
+          "Aborted!",
+          "Your booking was aborted successfully. If you didn't mean to cancel, you can always make a new booking.",
+          'Great',
+        )
+      }
+    )
+  }
+  preview(){
+    Notiflix.Report.info(
+      'Package preview',
+      "Thank you for your interest in this product. We are still working on generating package previews for all our products. Hang on tight and check back with us in a few weeks!",
+      'Sure!'
+    )
+  }
 
   ngOnDestroy(): void {
     this.editor.destroy();
